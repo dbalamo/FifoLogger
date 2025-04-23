c7ca1b7b159a89588be2809eddec9cbefeb53f0c
davide <davide@localhost.localdomain>
2025-04-23 15:27:39 +0200

    various updates

--- examples/simple-stdout.ts
+++ examples/simple-stdout.ts
@@ -5,7 +5,8 @@ console.log("TEST START");
 const testAttachedObj1 = {alfa:"beta", apple: 123}
 const testAttachedObj2 = ["gamma", "delta", "epsilon"]
 
-FifoLogger.log(LogLevel.DEBUG, "1 - TEST ME UP 1 2 3", testAttachedObj1, testAttachedObj2)
+FifoLogger.log(LogLevel.DEBUG, "this will not be visible, because the default log level is INFO")
+FifoLogger.log(LogLevel.INFO, "1 - TEST ME UP 1 2 3", testAttachedObj1, testAttachedObj2)
 FifoLogger.log(LogLevel.WARNING, "2 - Simple Message without attachments")
 
 console.log("ENDE");
